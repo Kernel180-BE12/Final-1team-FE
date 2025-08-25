3104dc5efc83783d21c9bd929c264a2a219ab5df
gywn3636 <gywn3636@gmail.com>
2025-08-25 18:27:49 +0900

    feat: 프로젝트 기본 구조 및 페이지 라우팅 설정

---
+++ src/pages/AgentPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AgentPage = () => {
+    return <div>에이전트에게 요청하는 첫 페이지</div>;
+};
+
+export default AgentPage;
---
+++ src/pages/SuggestionPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SuggestionPage = () => {
+    return <div>AI가 템플릿을 추천/생성하는 페이지</div>;
+};
+
+export default SuggestionPage;
